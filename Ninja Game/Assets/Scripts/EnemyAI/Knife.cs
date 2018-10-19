5cb5c7efd4b345f8a3b2eea1fc4165baccd8ec4b
pzachos <mariomanpaul@gmail.com>
2018-10-19 11:32:06 -0400

    Added Enemy AI scripts

---
+++ Ninja Game/Assets/Scripts/EnemyAI/Knife.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Knife : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb.AddForce((GameObject.FindWithTag("Player").transform.position - transform.position) * speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Ninja Game/Assets/Scripts/EnemyAI/Throw.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Throw : MonoBehaviour {
+
+    public GameObject knife;
+    public GameObject shuriken;
+
+    private bool isWaiting = false;
+    private int choice;
+
+    void Update()
+    {
+        if (isWaiting == false) {
+            isWaiting = true;
+            StartCoroutine(Timer());
+        }
+    }
+
+    IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(5);
+        choice = Random.Range(0, 2);
+        if (choice == 0)
+        {
+            Instantiate(knife);
+        }
+        else
+        {
+            //Instantiate(shuriken);
+            Instantiate(knife);
+        }
+        isWaiting = false;
+    }
+}
