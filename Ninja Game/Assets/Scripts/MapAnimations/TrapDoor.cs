07fc5cf669f5db3ec063a2fb02bbf63f37ed10d5
Chase Sparks <chaspa97@att.net>
2018-11-10 22:32:49 -0600

    Target dummy,doorway,nunchucks, shuriken model

---
+++ Ninja Game/Assets/Scripts/MapAnimations/TrapDoor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapDoor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.Rotate(Vector3.up * 90);
+    }
+    void OnTriggerEnter(Collider otherObj)
+    {
+        if (otherObj.gameObject.tag == "Enemy")
+        {
+            
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
