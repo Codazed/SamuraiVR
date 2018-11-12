cd5f50ff83cb40ec234d772ffc27a642a9f80065
Chase Sparks <chaspa97@att.net>
2018-11-12 17:51:16 -0600

    2 new songs we could maybe use?

--- Ninja Game/Assets/PlayMusic.cs
+++ Ninja Game/Assets/PlayMusic.cs
@@ -1,20 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[RequireComponent(typeof(GameObject))]
 public class PlayMusic : MonoBehaviour {
     public GameObject gameObjectToEnable;
     public GameObject Music2;
+    public GameObject Music3;
+    public GameObject Music4;
     private int choice;
     // Use this for initialization
     void Start () {
 
         //This script picks a random song from our playlist
-        choice = Random.Range(0, 2);
+        choice = Random.Range(0, 4);
         Debug.Log(choice);
         if (choice == 0)
         {
             gameObjectToEnable.SetActive(true);
+        }
+        else if (choice == 1)
+        {
+            Music3.SetActive(true);
+        }
+        else if (choice == 2)
+        {
+            Music4.SetActive(true);
         }
         else
         {
