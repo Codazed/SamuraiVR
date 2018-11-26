bb9eb63e121538359313facf9afa100931d03682
Chase Sparks <chaspa97@att.net>
2018-11-26 11:07:35 -0600

    Worked on sword spawner

---
+++ Ninja Game/Assets/Scripts/SwordSpawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(GameObject))]
+public class SwordSpawn : MonoBehaviour
+{
+    GameObject katana;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnCollisionEnter(Collision otherObj)
+    {
+        if (otherObj.gameObject.tag == "Head")
+        {
+            Instantiate(katana, transform.position, transform.rotation);
+            
+        }
+    }
+}
