444af395bf26e9e834182f7466d1fba78a930383
Chase Sparks <chaspa97@att.net>
2018-11-29 14:24:13 -0600

    Trapdoor enemy

---
+++ Ninja Game/Assets/Scripts/EnemyAI/Trapdoorenemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trapdoorenemy : MonoBehaviour {
+    int count = 0;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (count < 400) {
+            transform.Translate(Vector3.up * 2 * Time.deltaTime);
+            count = count + 1;
+        }
+        else if (count >= 800)
+        {
+            count = 0;
+        }
+        else if (count >=400)
+        {
+            transform.Translate(Vector3.down * 2 * Time.deltaTime);
+            count = count + 1;
+        }
+        
+        }
+}
