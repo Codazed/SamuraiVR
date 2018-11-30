abcd556488fe702efe4922d3dca891469f2a9c0e
Chase Sparks <42758063+SirElkinator@users.noreply.github.com>
2018-11-30 11:05:30 -0600

    Hp work

---
+++ Ninja Game/Assets/PlaneHealth.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneHealth : MonoBehaviour {
+
+    Color mat;
+    public PlayerHealth s;
+
+	// Use this for initialization
+	void Start () {
+        mat = GetComponent<MeshRenderer>().material.color;
+        s = GameObject.Find("SwordSpawn").GetComponent<PlayerHealth>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (s.hp == 5) {
+            mat.a = 0;
+        } else {
+            mat.a = 1f - (0.2f * s.hp);
+        }
+	}
+}
---
+++ Ninja Game/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+    public int hp = 5;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log("Health= " + hp);
+    }
+    void OnTriggerEnter(Collider otherObj) {
+        if (otherObj.gameObject.tag == "Shuriken") {
+            hp = hp - 1;
+           
+        }
+    }
+}
