8cf8ca64b92ebbb0a5f5fc13bb85e4a0ec193f17
Chase Sparks <chaspa97@att.net>
2018-11-30 23:08:16 -0600

    The alpha updates when damage is taken

--- Ninja Game/Assets/PlaneHealth.cs
+++ Ninja Game/Assets/PlaneHealth.cs
@@ -5,20 +5,24 @@ using UnityEngine;
 public class PlaneHealth : MonoBehaviour {
 
     Color mat;
+    float newAlpha;
     public PlayerHealth s;
 
 	// Use this for initialization
 	void Start () {
-        mat = gameObject.GetComponent<Renderer>().material.color;
+        mat = GetComponent<MeshRenderer>().material.color;
         s = GameObject.Find("SwordSpawn").GetComponent<PlayerHealth>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        //Debug.Log(s.hp);
+        Debug.Log(mat.a);
         if (s.hp == 5) {
-            mat.a = 0;
+            newAlpha = 0;
         } else {
-            mat.a = 1f - (0.2f * s.hp);
+             newAlpha = 1 - (0.2f * s.hp);
         }
-	}
+        GetComponent<Renderer>().material.color = new Color(GetComponent<Renderer>().material.color.r, GetComponent<Renderer>().material.color.g, GetComponent<Renderer>().material.color.b, newAlpha);
+    }
 }
